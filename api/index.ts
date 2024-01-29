0cf137fa607b9fa205ef1cb11b93c5aa33253569
George MacKerron <george@mackerron.co.uk>
2024-01-29 17:49:29 +0000

    Initial commit

---
+++ api/index.ts
@@ -0,0 +1,24 @@
+import { drizzle } from 'drizzle-orm/vercel-postgres';
+import { sql } from '@vercel/postgres';
+import {
+  pgTable,
+  serial,
+  text,
+} from 'drizzle-orm/pg-core';
+
+export const db = drizzle(sql);
+
+export const users = pgTable('users', {
+  id: serial('id').primaryKey(),
+  name: text('name').notNull(),
+});
+
+export default async (req: Request, ctx: any) => {
+  const result = db.select().from(users);
+  return new Response(JSON.stringify(result, null, 2));
+}
+
+export const config = {
+  runtime: 'edge',
+  regions: ['fra1'],  // fra1 = Frankfurt: pick the Vercel region nearest your Neon DB
+};
